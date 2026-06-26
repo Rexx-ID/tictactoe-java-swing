662015db23712d69003c2be01e56013cfc384068
Rexx-ID <satriautamafayyadh@gmail.com>
2026-06-27 00:34:40 +0700

    Hapus PW DB

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -7,7 +7,7 @@ public class DatabaseManager {
     // TODO: Change these values based on your database configuration
     private static final String URL = "jdbc:postgresql://localhost:5433/game_project";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "1c454sbb";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
